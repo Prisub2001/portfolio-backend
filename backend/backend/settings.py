9b5a9f553544ca2f736a83b2e7bed996456af2ed
Prisub2001 <priyanshusubbarwal@gmail.com>
2025-12-27 02:46:34 +0530

    chagne

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,16 +60,13 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
-# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-
-# EMAIL_HOST_USER = 'priyanshusubbarwal@gmail.com'
-# EMAIL_HOST_PASSWORD = 'fijjuazfamyjjzcn'
+EMAIL_HOST_USER = 'priyanshusubbarwal@gmail.com'
+EMAIL_HOST_PASSWORD = 'fijjuazfamyjjzcn'
 
 ROOT_URLCONF = 'backend.urls'
 
